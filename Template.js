f5d769be2f56da1bbfd92e36cbe6bbddfcdac4ee
LoganC <alclary@posteo.net>
2023-12-01 15:50:41 -0500

    Quick start template

---
+++ Template.js
@@ -0,0 +1,13 @@
+// Advent of Code 2023, Day
+
+const util = require("./util.js");
+let data = util.fileToArr("input.txt");
+console.log(data);
+
+// Step 1
+function step1(data) {}
+
+// // Step 2
+// function step2(data) {
+
+// }
